e7d01fb2efc8e2398398e0f4998bce6e1b08d2c2
juanHerrera <portador1051@gmail.com>
2023-04-12 16:34:04 -0400

    Blog and My notes v003

--- index.js
+++ index.js
@@ -5,8 +5,43 @@ const next = document.getElementById('btnNext')
 const menuShow = document.getElementById('menuShow')
 
 
-prevent.addEventListener('click', () => plusCarrusel(-1))
-next.addEventListener('click', () => plusCarrusel(1))
+window.addEventListener('DOMContentLoaded', () => {
+    window.scroll({top:0})
+    currentCarrusel(positionInitial);
+})
+
+
+
+// console.log( "console 1", document.querySelector('#btn_menu_show').getBoundingClientRect().bottom)
+window.addEventListener('scroll', () => {
+    let referenceElement = menuShow.getBoundingClientRect().bottom
+    let referenceElementWindow = document.querySelector('body').getBoundingClientRect().bottom
+    let positionNavbar = window.scrollY
+
+    // console.log(`elemento DOM menu ${parseInt(referenceElement)}`)
+    // console.log(`elemento Body ${parseInt(referenceElementWindow / 4) }`)
+    // console.log(`Barra posicionada en el docuemnto ${parseInt(positionNavbar)}`)
+
+    console.log(referenceElementWindow / 4 <= positionNavbar)
+
+    if(referenceElementWindow / 4 <= positionNavbar) {
+        document.querySelector('.header-nav').style.backgroundColor = 'var(--azul)'
+        return 
+    }
+    
+    document.querySelector('.header-nav').style.backgroundColor = 'initial'
+})
+
+const showConsole = () => {
+
+    const options = {
+        top: window.innerHeight,
+        left:0,
+        behavior: "smooth"
+    }
+
+    window.scroll(options)
+}
 
 buttonMenu.addEventListener('click', () => {
     ShowMenu()
@@ -30,10 +65,8 @@ function ShowMenu (){
 
 let positionInitial = 1;
 
-window.addEventListener('DOMContentLoaded', () => {
-    currentCarrusel(positionInitial);
-})
-
+prevent.addEventListener('click', () => plusCarrusel(-1))
+next.addEventListener('click', () => plusCarrusel(1))
 
 
 function plusCarrusel(n)  {
@@ -46,11 +79,13 @@ function currentCarrusel(n) {
 
 function carruselController (numberGenerated) {
     let i;
+
     let carrusel = [...document.querySelectorAll('.carrusel .hero')]
     let banner = [...document.querySelectorAll('.carrusel .round')]
 
     if(numberGenerated > carrusel.length){positionInitial = 1}
     if(numberGenerated < 1){positionInitial = carrusel.length}
+
 
     for( i = 0 ; i < carrusel.length; i++){
         carrusel[i].classList.remove('hero_view')
