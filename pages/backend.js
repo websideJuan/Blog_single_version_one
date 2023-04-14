fc7b1437bb4140e9e790b2ccc6c0cd6efdda4cd8
juanHerrera <portador1051@gmail.com>
2023-04-14 15:52:06 -0400

    Blog and My notes v006

--- pages/backend.js
+++ pages/backend.js
@@ -8,20 +8,28 @@ function backendNote () {
 
     DOMrender.innerHTML = ''
 
-    const consoleLogFrom = (e) => {
-        if(e.target.parentElement.parentElement.parentElement.classList.contains('activate')){  
-            e.target.parentElement.parentElement.parentElement.classList.remove('activate')
-            return
-        }
-        e.target.parentElement.parentElement.parentElement.classList.add('activate')
     
-    }
     
     containerRender.innerHTML = `<div class="docs activate">
-        <div class="nav_docs">Hello world</div>
+        <div class="nav_docs">
+            <ul class="nav_item_docs">
+                <li>
+                    <a href="#">home</a>
+                </li>
+
+                <li>
+                    <a href="#">home</a>
+                </li>
+
+                <li>
+                    <a href="#">home</a>
+                </li>
+            </ul>
+        </div>
+        
         <div class="header_docs">
             <div class="hero_docs">
-                <h2>Titulo One</h2>
+                <h2 class="title_header_docs">Titulo One</h2>
 
                 <button class="btn" id="btn_hero" >
                     <img href="../assets/icons/navegation.svg" alt="none"/>
@@ -29,12 +37,6 @@ function backendNote () {
             </div>
 
             <div class="container_docs">
-                <div class="cards_docs">
-                </div>
-
-                <div class="cards_docs">
-                </div>
-
                 <div class="cards_docs">
                 </div>
             </div>
@@ -46,8 +48,30 @@ function backendNote () {
     
 
     let btn_hero_docs = DOMrender.querySelector('#btn_hero')
+    btn_hero_docs.addEventListener('click', (e) => showMenuDocs(e))
 
-    btn_hero_docs.addEventListener('click', (e) => consoleLogFrom(e))
+
+
+
+    const showMenuDocs = ({target:{parentElement}}) => {
+
+
+        setShowMenuDocs(parentElement.parentElement.parentElement.parentElement)
+       
+
+    
+    }
+
+}
+
+
+const setShowMenuDocs = (reference) => {
+    if(reference.classList.contains('activate')){  
+        reference.classList.remove('activate')
+        return
+    }
+
+    reference.classList.add('activate')
 }
  
 
